2b1a81083d13785645bb500b501c26be7de36c4e
root <root@kali.kali>
2026-03-13 14:18:16 -0500

    Bit of destructuring

--- change_mac.py
+++ change_mac.py
@@ -1,34 +1,14 @@
 #/usr/bin/env python
 
-import subprocess
-import optparse
-import re
+from helpers.utils import get_arguments, change_mac
 
-def is_valid_mac(mac: str) -> bool:
-    pattern = r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
-    return bool(re.fullmatch(pattern, mac))
-
-def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address.")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address.")
-    parser.add_option("-v", "--view", dest="view", help="enter True for View result")
-    (opt, args) = parser.parse_args()
-    if not opt.interface:
-        parser.error("[-] Please specify an interface, use --help for more info")
-    elif not opt.new_mac:
-        parser.error("[-] Please specify a new MAC Address, use --help for more info")
-    is_valid = is_valid_mac(opt.new_mac)
-    if not is_valid:
-        parser.error("[-] Please use the correct syntax for the new MAC address [XX:XX:XX:XX:XX:XX]")
-    return opt
-
-def change_mac(interface, new_mac, view):
-    print("[+] Changing MAC address for " + interface + " to " + new_mac)
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
-    if view: subprocess.call(["ifconfig", interface])
+# Change_mac simple algorithm
+# Check if Mac address has changed
+# Setups:
+# 1. Execute and read ifconfig
+# 2. Read the MAC address from input
+# 3. Check if MAC in ifconfig is what the user requested
+# 4. Print appropriate message
 
 options = get_arguments()
 change_mac(options.interface, options.new_mac, options.view)
---
+++ helpers/utils.py
@@ -0,0 +1,28 @@
+import subprocess
+import optparse
+import re
+
+def get_arguments():
+    def is_valid_mac(mac: str) -> bool:
+        pattern = r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
+        return bool(re.fullmatch(pattern, mac))
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address.")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address.")
+    parser.add_option("-v", "--view", dest="view", help="enter True for View result")
+    (opt, args) = parser.parse_args()
+    if not opt.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not opt.new_mac:
+        parser.error("[-] Please specify a new MAC Address, use --help for more info")
+    is_valid = is_valid_mac(opt.new_mac)
+    if not is_valid:
+        parser.error("[-] Please use the correct syntax for the new MAC address [XX:XX:XX:XX:XX:XX]")
+    return opt
+
+def change_mac(interface: str, new_mac: str, view: bool):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    if view: subprocess.call(["ifconfig", interface])
