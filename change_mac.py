c47fbe359d09dc0e48d85943e0593b8a057a7d05
root <hackenstance@proton.me>
2026-03-14 15:49:11 -0500

    Refactored to run all version of Python, notes added

--- change_mac.py
+++ change_mac.py
@@ -1,9 +1,11 @@
 #/usr/bin/env python
-
 from helpers.utils import get_arguments, is_valid_mac, change_mac, check_mac_address_updated, get_current_mac
 
+# Get the interface and new MAC values from the user.  Store them in options.
 options = get_arguments(is_valid_mac)
+# Update the MAC Address for the specified interface.
 change_mac(options.interface, options.new_mac, options.view)
+# Run a test to make sure the MAC Address has been updated: That the current address matches the requested one.
 check_mac_address_updated(options, get_current_mac)
 
 
--- helpers/utils.py
+++ helpers/utils.py
@@ -1,14 +1,18 @@
+#/usr/bin/env python
 import subprocess
 import optparse
 import re
 
-def is_valid_mac(mac: str) -> bool:
+# Check to see if a MAC Address has a valid format.
+def is_valid_mac(mac):
     pattern = r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
-    return bool(re.fullmatch(pattern, mac))
+    return bool(re.search(pattern, mac))
 
+# Return the MAC Address of a specified Interface
 def get_current_mac(interface):
     # 1. Execute and read ifconfig
-    ifconfig_result = subprocess.check_output(["ifconfig", interface], text=True)
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    ifconfig_result = ifconfig_result.decode("utf-8")
     print("Here is our current ifconfig for " + interface + " " + ifconfig_result)
     # 2. Read the MAC address from input, use Regex
     mac_address_search_result = re.search(r"(\w\w:){5}(\w\w)", ifconfig_result)
@@ -16,20 +20,22 @@ def get_current_mac(interface):
         current_mac = mac_address_search_result.group(0)
         return current_mac
     else:
-        print("[-] Could not read the MAC Address")
+        print("[-] Something went wrong. Could not read the MAC Address")
         return None
 
-
-def check_mac_address_updated(options, get_mac) -> str:
+# Check the MAC Address has been updated to the NEW MAC Address.
+def check_mac_address_updated(options, get_mac):
     current_mac = get_mac(options.interface)
     # 3. Check if MAC in ifconfig is what the user requested
     if options.new_mac == current_mac:
-        # 4. Print appropriate message
-        print("Requested MAC: " + options.new_mac + ". Current MAC Address: " + current_mac)
-        print("MAC Address successfully updated.")
+    # 4. Print appropriate message
+        print("MAC Address successfully changed to: " + current_mac)
     else:
         print("No MAC Address update occurred")
 
+# Get a value from the user for the Interface.
+# Get a value from the user for the new MAC address.
+# Check the new MAC Address provided is in a valid format.
 def get_arguments(is_valid_new):
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address.")
@@ -40,12 +46,15 @@ def get_arguments(is_valid_new):
         parser.error("[-] Please specify an interface, use --help for more info")
     elif not opt.new_mac:
         parser.error("[-] Please specify a new MAC Address, use --help for more info")
+    # Check if the MAC Address is valid.  Uses an external function.
     is_valid = is_valid_new(opt.new_mac)
     if not is_valid:
-        parser.error("[-] Please use the correct syntax for the new MAC address [XX:XX:XX:XX:XX:XX]")
+        parser.error("[-] Please use the correct format for the new MAC Address [XX:XX:XX:XX:XX:XX]")
     return opt
 
-def change_mac(interface: str, new_mac: str, view: bool):
+# Update the MAC Address for the specified interface.
+# Execute ifconfig if required to preview changes (redundant probably)
+def change_mac(interface, new_mac, view):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
