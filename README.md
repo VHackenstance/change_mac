89e30809b282a1fbbd97f9980fc6be93c277c584
Vhackenstance <hackenstance@proton.me>
2026-04-14 03:14:12 -0500

    Housekeeping

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h1>change_mac.py</h1>
-<p>Python script, works with all versions currently.</p>
-<p>Create and tested in Linux, no compatibility Windows or MacOS currently.</p>
+<p>Python script, works with all Python versions currently.</p>
+<p>Create and tested in Linux, no compatibility Windows or MacOS.</p>
 <p>Basic help is available if you run the script with --help</p>
 <h4>$ python mac_changer.py --help</h4>
 <p>To run the script please provide arguments in the following format. 
@@ -10,7 +10,7 @@ python[#] change_mac.py -i [INTERFACE] -m [XX:XX:XX:XX:XX:XX]
 </h3>
 <h4>Press enter and three things should happen</h4>
 <ol>
-<li>Confirmation of the interface and new MAC you requested.</li>
+<li>Confirmation of the <b>interface</b> and <b>new MAC</b> you requested.</li>
 <li>ifconfig will execute for your interface, to provide more details.</li>
 <li>You will get confirmation of the update.  Or an error message.</li>
 </ol>
---
+++ helpers/utils_commented.py
@@ -0,0 +1,49 @@
+#/usr/bin/env python
+import subprocess
+import argparse
+import re
+
+def is_valid_mac(mac):
+    pattern = r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
+    return bool(re.search(pattern, mac))
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    ifconfig_result = ifconfig_result.decode("utf-8")
+    mac_address_search_result = re.search(r"(\w\w:){5}(\w\w)", ifconfig_result)
+    if mac_address_search_result:
+        current_mac = mac_address_search_result.group(0)
+        return current_mac
+    else:
+        print("[-] Something went wrong. Could not read the MAC Address")
+        return None
+
+def check_mac_address_updated(options, get_mac):
+    current_mac = get_mac(options.interface)
+    if options.new_mac == current_mac:
+        print("\n[+] MAC Address successfully changed to: " + current_mac)
+    else:
+        print("No MAC Address update occurred")
+
+def get_arguments(is_valid_new):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interface to change MAC address.")
+    parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address.")
+    parser.add_argument("-v", "--view", dest="view", help="enter True for View result")
+    options = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC Address, use --help for more info")
+    is_valid = is_valid_new(options.new_mac)
+    if not is_valid:
+        parser.error("[-] Please use the correct format for the new MAC Address [XX:XX:XX:XX:XX:XX]")
+    return options
+
+def change_mac(interface, new_mac, view):
+    current_mac = get_current_mac(interface)
+    print("\n[+] Changing MAC address for " + interface + " from " + current_mac + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    if view: subprocess.call(["ifconfig", interface])
